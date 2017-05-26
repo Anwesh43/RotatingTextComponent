9065ccc64c760957102e9cb2663df46759f8377f
Anwesh43 <anweshthecool0@gmail.com>
2017-05-26 15:10:56 +0530

    drawing text and the colored card

--- rotating-text-component-div.js
+++ rotating-text-component-div.js
@@ -1,6 +1,22 @@
 class RotatingTextComponent extends HTMLElement{
     constructor() {
         super()
-        
+        this.color = this.getAttribute("color")
+        this.text = this.getAttribute("text")
+        this.tags = this.getAttribute("tags")
+        this.img = document.createElement('img')
+    }
+    draw() {
+        const canvas = document.createElement('canvas')
+        canvas.width = window.innerWidth
+        canvas.height = window.innerHeight
+        const context = canvas.getContext('2d')
+        context.clearRect(0,0,canvas.width,canvas.height)
+        context.fillStyle = this.color
+        context.fillRect(0,0,canvas.width,canvas.height)
+        context.fillStyle = 'white'
+        context.font = context.font.replace(/\d{2}/,`${window.innerWidth/15}`)
+        const tw = context.measureText(this.text).width
+        context.fillText(this.text,canvas.width/2-tw,canvas.height/2)
     }
 }
